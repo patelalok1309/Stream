8f32a3aa82c056c7900368781bda7e179cd312f0
ALOK PATEL <alokdholu10@gmail.com>
2025-02-19 15:40:28 +0530

    postgress setup with drizzle ORM

---
+++ src/db/schema.ts
@@ -0,0 +1,21 @@
+import {
+  pgTable,
+  text,
+  uuid,
+  timestamp,
+  uniqueIndex,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerkId").unique().notNull(),
+    name: text("name").notNull(),
+    // TODO : add banner fields
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  },
+  (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)] // indexing clerk_id
+);
